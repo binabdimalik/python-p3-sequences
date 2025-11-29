62e712d66a3d05bef43b1b624bf9823116d3f3b2
Abdimalik <binabdimalikranger@gmail.com>
2025-11-29 13:50:29 +0300

    resolved

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    elif length == 1:
+        print([0])
+        return
+    
+    seq = [0, 1]
+
+    while len(seq) < length:
+        next_val = seq[-1]  + seq[-2]
+        seq.append(next_val)
+
+    print(seq)
